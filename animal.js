ac9e049b19140d1478a57a1947ac17b0b1eba161
Kniaziev <knyazev.alexandr@gmail.com>
2015-03-09 11:04:05 +0200

    first commit

---
+++ animal.js
@@ -0,0 +1,18 @@
+﻿var Animal = {
+    constructor: function (name) {
+        this.name = name;
+        return this;
+    },
+    getName: function () {
+        console.log(this.name);
+    }
+};
+
+var dog = Object.create(Animal);
+
+dog.constructor("Goofy");
+dog.Barks = function () {
+    console.log("Dog " + dog.name + " is barking");
+};
+dog.Barks();
+dog.getName();
